1fa218f0b4d12a44ab7fd5288fb2e7938dffcfe4
soumyakundu <soumya.kundu@stanford.edu>
2019-03-01 16:25:44 -0800

    More minor formatting changes

--- atac_seq_pipeline_utils/run.sh
+++ atac_seq_pipeline_utils/run.sh
@@ -10,13 +10,13 @@ SH_SCRIPT=/scratch/groups/akundaje/annashch/testing/template_submission.sh
 for type in $PROJECT_ROOT/AD/*; do
     for region in $type/*; do
         for file in $region/*; do
-	        prefix=$(basename $file .json)
-	        WORK_DIR=$RUN_ROOT/$type/$region/$prefix
-	        mkdir -p $WORK_DIR && cd $WORK_DIR
-	        ln -s $file
-	        sbatch --partition akundaje --mem=30G \
-		    -o $LOG_DIR/$prefix.o -e $LOG_DIR/$prefix.e \
-		    $SH_SCRIPT $file
+            prefix=$(basename $file .json)
+            WORK_DIR=$RUN_ROOT/$type/$region/$prefix
+            mkdir -p $WORK_DIR && cd $WORK_DIR
+            ln -s $file
+            sbatch --partition akundaje --mem=30G \
+                -o $LOG_DIR/$prefix.o -e $LOG_DIR/$prefix.e \
+                $SH_SCRIPT $file
         done
     done
 done
