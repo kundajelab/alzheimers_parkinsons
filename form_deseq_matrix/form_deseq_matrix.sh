2735480e79f6f9a54d577e39174fbab73a8a18f5
anna shcherbina <annashcherbina@gmail.com>
2019-03-21 15:11:24 -0700

    added code to truncate concatenated peak set to 200 bp

--- form_deseq_matrix/form_deseq_matrix.sh
+++ form_deseq_matrix/form_deseq_matrix.sh
@@ -7,7 +7,9 @@ for peak_file in `cut -f2 narrowPeak.files.txt`
 do
     zcat $peak_file >> naive_overlap.optimal_set.bed
 done
-bedtools sort -i naive_overlap.optimal_set.bed > naive_overlap.optimal_set.sorted.bed
+#truncate peaks to 200 bp around summit
+python truncate_merged_peak_file_to_200bp_around_summit.py --input_bed naive_overlap.optimal_set.bed --summit_flank 100 --outf naive_overlap.optimal_set.200.bed
+bedtools sort -i naive_overlap.optimal_set.200.bed > naive_overlap.optimal_set.sorted.bed
 bedtools merge -i naive_overlap.optimal_set.sorted.bed > naive_overlap.optimal_set.sorted.merged.bed
 
 echo "generated merged peak file!"
