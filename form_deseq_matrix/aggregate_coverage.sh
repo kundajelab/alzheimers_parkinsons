e8b0f835140e832cf3deaca9742dc2929a889ba6
anna shcherbina <annashcherbina@gmail.com>
2019-03-22 08:36:27 -0700

    clean up

--- form_deseq_matrix/aggregate_coverage.sh
+++ form_deseq_matrix/aggregate_coverage.sh
@@ -1,5 +1,12 @@
 paste counts.*.txt > adpd.atac.counts.txt
 #clean up temporary files                                                                                                                                                                                          
-rm counts.*.txt
-paste naive_overlap.optimal_set.sorted.merged.bed adpd.atac.counts.txt > tmp
-mv tmp adpd.atac.counts.txt
+
+echo -e $'chrom\tstart\tend' > index
+cat index naive_overlap.optimal_set.sorted.merged.bed > tmp1
+paste tmp1 adpd.atac.counts.txt > tmp2
+mv tmp2 adpd.atac.counts.txt
+
+#clean up
+rm tmp1
+rm index
+#rm counts.*.txt
