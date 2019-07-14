b8a09e63724430425e884a221dc5c694b7edea26
Anna Shcherbina <annashcherbina@gmail.com>
2019-07-14 16:44:38 -0700

    getting ambiguous peaks

---
+++ ambig_peaks/get_ambiguous.sh
@@ -0,0 +1,17 @@
+num_samples=`cat idr.optimal.narrowPeaks.txt| wc -l`  
+echo $num_samples 
+for i in `seq 1 $num_samples`
+do
+    echo $i
+    a_file=`head -n $i overlap.optimal.narrowPeaks.txt | tail -n1`
+    b_file=`head -n $i idr.optimal.narrowPeaks.txt | tail -n1`
+    cur_sample=`head -n $i samples.txt | tail -n1`
+    echo "a_file:$a_file"
+    echo "b_file:$b_file" 
+    bedtools intersect -v -a $a_file -b $b_file > $cur_sample.ambiguous.bed
+    bgzip $cur_sample.ambiguous.bed
+    echo $cur_sample.ambiguous.bed.gz 
+done
+
+
+
