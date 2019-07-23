f90dd9ca9770f5f8a55a712a2ad460340304bd40
soumyakundu <soumya.kundu@stanford.edu>
2019-07-23 02:52:46 +0000

    Trained regression models and completed SNP interpretation for MAPT locus

---
+++ models/AD_CTRL.sh
@@ -0,0 +1,4 @@
+./crossvalid_mean.sh AD_CTRL_CAUD 1
+./crossvalid_mean.sh AD_CTRL_PARL 1
+./crossvalid_mean.sh AD_CTRL_SMTG 1
+./crossvalid_mean.sh AD_CTRL_HIPP 1
---
+++ models/PD_CTRL.sh
@@ -0,0 +1,6 @@
+./crossvalid_mean.sh PD_CTRL_CAUD 0
+./crossvalid_mean.sh PD_CTRL_MDFG 0
+./crossvalid_mean.sh PD_CTRL_PTMN 0
+./crossvalid_mean.sh PD_CTRL_MDTG 0
+./crossvalid_mean.sh PD_CTRL_HIPP 0
+./crossvalid_mean.sh PD_CTRL_SUNI 0
