bd6954267a594b0b76e9dc818fb0a61f50d9ba19
anna shcherbina <annashcherbina@gmail.com>
2020-01-07 12:57:42 -0800

    cleanup

--- pseudorep_check/split_groups.sh
+++
@@ -1,5 +0,0 @@
-for f in `cut -f1 groups.txt`
-do
-    echo "field:"$f
-    grep $f tagAlign.files.txt | cut -f2 >> t.$f
-done
--- pseudorep_peaks_supported_by_30percent_of_bioreps_in_same_region/counts_all/get_tagalign_coverage_over_merged_peak_file.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash 
-bedtools coverage -counts -a $1 -b $2 | cut -f4 >> counts.$3.txt
