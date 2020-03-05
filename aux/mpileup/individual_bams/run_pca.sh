3044d26d9cdbfc2c681b4d41eb50ad533d7ba591
Anna Shcherbina <annashcherbina@gmail.com>
2020-03-04 23:43:54 -0800

    pca analysis incorporating 1000 genomes phase 3 individual genotypesk, converted to bed files, filtered to common snps also present in ATAC-seq cohort

--- aux/mpileup/individual_bams/run_pca.sh
+++ aux/mpileup/individual_bams/run_pca.sh
@@ -1,1 +1,3 @@
-plink2 --pca 5 --allow-extra-chr --maf 0.05 --bfile /oak/stanford/groups/akundaje/projects/alzheimers_parkinsons/mpileup/individual_bams/merged/merged --out /oak/stanford/groups/akundaje/projects/alzheimers_parkinsons/mpileup/individual_bams/pca/adpd.merged.pca
+#!/bin/bash
+#plink2 --pca 5 --allow-extra-chr --maf 0.05 --bfile /oak/stanford/groups/akundaje/projects/alzheimers_parkinsons/mpileup/individual_bams/merged/merged --out /oak/stanford/groups/akundaje/projects/alzheimers_parkinsons/mpileup/individual_bams/pca/adpd.merged.pca
+plink2 --pca 5 --allow-extra-chr --maf 0.05 --bfile /oak/stanford/groups/akundaje/projects/alzheimers_parkinsons/mpileup/individual_bams/merged/common.filtered --out /oak/stanford/groups/akundaje/projects/alzheimers_parkinsons/mpileup/individual_bams/pca/adpd.1kg.merged.common.filtered.pca
