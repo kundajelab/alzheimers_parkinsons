3dde7ea243aaadb3044b14f9925fc20aa923e0d1
anna shcherbina <annashcherbina@gmail.com>
2020-03-20 15:55:35 -0700

    dopaminergic preprocessing

---
+++ single_cell_atacseq_preprocessing/pseudobulk/granges_to_tagAlign.dopa_neurons.sh
@@ -0,0 +1,3 @@
+Rscript granges_to_tagAlign.R /oak/stanford/groups/akundaje/projects/alzheimers_parkinsons/dopa_neurons_cluster_10/DopaNeurons_Cluster10_AllFrags_projSUNI2.rds /oak/stanford/groups/akundaje/projects/alzheimers_parkinsons/dopa_neurons_cluster_10/DopaNeurons_Cluster10_AllFrags_projSUNI2.tagAlign.gz 99 
+
+
