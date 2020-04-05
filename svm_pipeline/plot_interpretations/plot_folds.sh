ba65f1452d33e8361d9750ebefd1132ba4ec6e08
anna shcherbina <annashcherbina@gmail.com>
2020-04-05 15:25:23 -0700

    updated plotting scripts for gkmexplain values

--- svm_pipeline/plot_interpretations/plot_folds.sh
+++ svm_pipeline/plot_interpretations/plot_folds.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-#task_order: dnase_c dnase_v sw480 hct116 colo205 
-gkmexplain_prefix=/oak/stanford/groups/akundaje/projects/GECCO/SVM/gkmexplain_scores/aggregate_nodups/scores
-deepshap_prefix=/srv/scratch/annashch/gecco/interpret_cnn/deepshap
-outf_prefix=/srv/scratch/annashch/gecco/plots
+gkmexplain_prefix=/oak/stanford/groups/akundaje/projects/alzheimers_parkinsons/dopa_neurons_cluster_10/gkmexplain/outputs_from_gkmexplain/gkmexplain.
+outf_prefix=/oak/stanford/groups/akundaje/projects/alzheimers_parkinsons/dopa_neurons_cluster_10/gkmexplain_plots
 python plot_folds.py --snpinfo toplot.txt \
-       --tasks dnase_c dnase_v sw480 hct116 colo205 \
        --gkmexplain_prefix $gkmexplain_prefix \
-       --gkmexplain_suffix .txt.filtered \
-       --deepshap_pickle_classification_prefix $deepshap_prefix/deepshap.classification. \
-       --deepshap_pickle_regression_prefix $deepshap_prefix/deepshap.regression. \
+       --gkmexplain_suffix .txt \
        --outf_prefix $outf_prefix \
+       --flank 500 \
        --plot_start_base 400 \
        --plot_end_base 600 \
        --snp_pos 501
