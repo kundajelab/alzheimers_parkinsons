a232c5145c6a0f549c7c63015af31552532bbe98
soumyakundu <soumya.kundu@stanford.edu>
2020-05-30 15:58:10 -0700

    Updated new SNP scoring notebooks and analysis of RASQUAL results after generating 10x permutation q-values

--- clusters_gkmsvm/spawn_notebooks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in {2..24}
-    do
-        echo $i
-        cp /users/soumyak/alzheimers_parkinsons/snp_scoring_notebooks/Cluster1.ipynb /users/soumyak/alzheimers_parkinsons/snp_scoring_notebooks/Cluster${i}.ipynb
-    done
-
